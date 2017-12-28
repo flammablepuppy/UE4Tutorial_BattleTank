441ef9cea4bd06ac7fa32beecd714c8a436595ef
Phil <flammablepuppy@gmail.com>
2017-12-28 14:43:19 -0500

    Created scaffolding in player controller for aiming the barrel through the crosshair.

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "BattleTank.h"
 #include "TankPlayerController.h"
 
 void ATankPlayerController::BeginPlay()
@@ -14,9 +13,16 @@ void ATankPlayerController::BeginPlay()
 	}
 	else
 	{
-		UE_LOG(LogTemp, Warning, TEXT("%s is currently possessed."), *ControlledTank->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("%s is currently possessed by player."), *ControlledTank->GetName());
 	}
-	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"))
+}
+
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	// PASSED CHECK: UE_LOG(LogTemp, Warning, TEXT("Ticking."))
+	// Aim at crosshair
+	AimTowardCrosshair();
 }
 
 ATank* ATankPlayerController::GetControlledTank() const
@@ -24,3 +30,30 @@ ATank* ATankPlayerController::GetControlledTank() const
 	return Cast<ATank>(GetPawn());
 }
 
+void ATankPlayerController::AimTowardCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+	
+	FVector HitLocation; // Out parameter
+		if (GetSightRayHitLocation(HitLocation))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+		// TODO Tell controlled tank to aim at this point.
+
+	}
+}
+
+// Get world location of linetrace through crosshair
+bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
+{
+	// Find the crosshair position
+	int32 ViewportX; int32 ViewportY;
+	GetViewportSize(ViewportX, ViewportY);
+
+	auto ScreenLocation = FVector2D(ViewportX * CrosshairX, ViewportY * CrosshairY);
+	// PASSED CHECK: UE_LOG(LogTemp, Warning, TEXT("ScreenLocation: %s"), *ScreenLocation.ToString());
+
+	// De-project the crosshair screen position
+	// Linetrace along that direction, see what we hit (to a max range)
+	return true;
+}
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -15,8 +15,19 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
-public:
-	ATank * GetControlledTank() const;
-	
+private:
 	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	ATank * GetControlledTank() const;
+	void AimTowardCrosshair();  // Move the tank barrel to point under crosshair.
+	bool GetSightRayHitLocation(FVector& HitLocation) const;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairX = 0.5;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairY = 0.3333;
+
+
 };
