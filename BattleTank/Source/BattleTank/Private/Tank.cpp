4c045d6b5038a8afd7f0ccbc0f7160bcaea24a98
Phil <flammablepuppy@gmail.com>
2018-01-01 11:31:51 -0500

    TankAimingComponent added and working.

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -9,12 +9,13 @@ ATank::ATank()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// No need to protect pointers added at construction
+	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("AimComponent"));
 }
 
 void ATank::AimAt(FVector HitLocation)
 {
-	auto OurTankName = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%s is aming at %s"), *OurTankName, *HitLocation.ToString());
+	TankAimingComponent->AimAt(HitLocation);
 }
 
 // Called when the game starts or when spawned
